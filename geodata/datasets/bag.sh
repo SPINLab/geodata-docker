91771eb12c334eb75d5ed78da07406709bfa092c
reinvantveer <rein.van.t.veer@geodan.nl>
2018-11-07 15:17:04 +0100

    refactor to use from image rather than dockerfile

--- geodata/datasets/bag.sh
+++ geodata/datasets/bag.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-
 set -ex
 
-psql --dbname="geodata" <<-'EOSQL'
-  CREATE EXTENSION IF NOT EXISTS POSTGIS;
-  CREATE USER kademo;
-  ALTER DATABASE geodata OWNER TO kademo;
-EOSQL
+apt-get update && apt-get install -y curl
+curl -L -o ./bag.backup https://data.nlextract.nl/bag/postgis/bag-laatst.backup
+
+su postgres <<'EOF'
+  createdb geodata -U postgres --no-password
+  psql --dbname="geodata" -c "CREATE EXTENSION IF NOT EXISTS POSTGIS;"
+  psql --dbname="geodata" -c "CREATE USER kademo;"
+  psql --dbname="geodata" -c "ALTER DATABASE geodata OWNER TO kademo;"
+EOF
 
 echo "BAG database restore procedure started, wait for completion to connect to the database..."
 time pg_restore \
--- geodata/initdb/init.sh
+++ geodata/initdb/init.sh
@@ -24,10 +24,12 @@ for DB in template_postgis "$POSTGRES_DB"; do
 EOSQL
 done
 
+echo 'Creating database'
 createdb geodata -U postgres --no-password
 
 IFS=',' read -r -a GDS <<< "$GEODATASETS"
 
+chmod +x /datasets/*.sh
 for dataset in ${GDS[@]}; do
   bash /datasets/${dataset}.sh
 done
